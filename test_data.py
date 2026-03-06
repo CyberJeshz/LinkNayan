3d7926169b85c7b6e8bcdaf7cf250ef319d3c194
Jeshz-Dev <jezreeleguillermo@gmail.com>
2026-03-06 16:26:31 +0800

    minor rewriting

--- test_data.py
+++ test_data.py
@@ -1,31 +1,12 @@
 import json
 from pathlib import Path
 
-def testJson(filepath):
+def test_json(filepath):
     try:
-        with open(filepath, "r") as f:
-            data = json.load(f)
-        print(f"{filepath.name} is valuid.")
-        return True
-    except json.JSONDecodeError as e:
-        print(f"{filepath.name} has error: {e}")
+        with open(filepath, "r")as file:
+            data = json.load(file)
+            print(f"File name: {file.name} is valid")
+            return True
+    except Exception as e:
+        print(f"Error: {e}")
         return False
-    except FileNotFoundError as e:
-        print(f"{filepath.name} not found.")
-        return False
-    
-
-data_dir = Path("data")
-files = ["hotlines.json", "professionals.json", "resources.json"]
-
-print("Testing data files")
-all_valid = True
-for filename in files:
-    if not testJson(data_dir / filename):
-        all_valid = False
-
-
-if all_valid:
-    print("Files are valid")
-else:
-    print("Files have error.")
